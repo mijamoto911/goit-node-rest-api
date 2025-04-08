ef2c6efe218c14207b4cb70dc696fd9cf15510ee
Eduard Schumacher <107512513+mijamoto911@users.noreply.github.com>
2025-04-08 13:55:41 +0200

    add .env.example and middlewares

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -5,18 +5,16 @@ import {
   createContactSchema,
   updateContactSchema,
 } from '../schemas/contactsSchemas.js';
+import isEmptyBody from '../middlewares/isEmptyBody.js';
 
 const contactsRouter = express.Router();
 
 contactsRouter.get('/', contactsController.getAllContacts);
 contactsRouter.get('/:id', contactsController.getOneContact);
-contactsRouter.post(
-  '/',
-  validateBody(createContactSchema),
-  contactsController.createContact
-);
+contactsRouter.post('/', isEmptyBody, contactsController.createContact);
 contactsRouter.put(
   '/:id',
+  isEmptyBody,
   validateBody(updateContactSchema),
   contactsController.updateContact
 );
